a7a98b546fb48c4b774345596253afd4890eaea2
Daniel Kellmeyer <dsk96139@creighton.edu>
2019-09-28 16:49:18 -0500

    car loan
    Forgot to create a new branch

---
+++ FixItLevelUp3.appstudio/forms/loanPayments/loanPayments.js
@@ -0,0 +1,31 @@
+let carResult = 0
+
+  function carLoanPayments(a, t, i) {
+  carResult = (a*i)/(t*12);
+}
+
+let decision = prompt("Please enter 'Car' or 'Home' for specific monthly loan payment calculations.")
+let amount = prompt("Enter the amount of your loan.")
+let timeFrame = prompt("Enter the time frame of your loan.")
+let interestRate = prompt("Enter the interest rate of your loan.")
+
+if (decision === 'Car' || decision === 'car') {
+  carLoanPayments(amount, timeFrame, interestRate);
+  alert('A car loan for '+ amount +' over '+ timeFrame +' years at '+ interestRate +' interest would have a monthly payment of '+ carResult)
+}
+
+  
+  
+  /*
+function homeLoanPayments(a2, t2, i2) {
+  alert('A home loan for '+ arugment[0] +'over '+ arugment[1] +'years at '+ arugment[2] +'interest would have a monthly payment of '+ return (argument[0] * argument[2]) / argument[1])
+  }
+
+  
+  alert('A car loan for '+ arugment[0] +'over '+ arugment[1] +'years at '+ arugment[2] +'interest would have a monthly payment of '+ carLoanPayments())
+  
+  
+  function carLoanPayments(a, t, i) {
+  carResult = (a*i)/t;
+}
+*/
